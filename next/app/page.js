b5f161decb40c49ed7ad33527d0934d1528d6b87
Arthur  Silveira <arthurmarques2148@gmail.com>
2025-08-15 00:34:36 +0000

    Criando Perfil

--- next/app/page.js
+++ next/app/page.js
@@ -1,6 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+import { Profile } from "@/components/Profile";
 
 export default function Home() {
-  return <div>Hello World!</div>;
-}
+  return (
+    <div>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </div>
+  );
+} 
